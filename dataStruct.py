47c338e0777465ca558ae85e2447cc8f397f94aa
test <test@qq.com>
2020-06-11 05:03:48 +0700

    新增队列定义

--- dataStruct.py
+++ dataStruct.py
@@ -17,4 +17,17 @@ class Stack:
     def size(self):
         return len(self.items)
 
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(self,item):
+        self.items.append(item)
+    def dequeeu(self):
+        self.items[0]
+    def isEmpty(self):
+        return self.items == []
+    def size(self):
+        return len(self.items)
+
         
